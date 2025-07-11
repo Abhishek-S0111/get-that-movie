2f77bfbe17262622b268f43725d678791e16fca3
Abhishek-S0111 <160207083+Abhishek-S0111@users.noreply.github.com>
2025-07-11 23:37:22 +0530

    troubleshooting streamlit

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ import streamlit as st
 from recommender import get_recommendations
 
 movie = st.text_input('Enter a movie name: ')
-if movie:
-    recommendations = get_recommendations(movie)
-    st.write(f"Five Movies Similar to '{movie}' are :/n")
-    for rec in recommendations:
-        st.write('- ', rec)
+
+st.write("Brother, can i have some oats?")
+# if movie:
+#     recommendations = get_recommendations(movie)
+#     st.write(f"Five Movies Similar to '{movie}' are :/n")
+#     for rec in recommendations:
+#         st.write('- ', rec)
