37be38a8e6e18bc9d8c59d49fc5081f3195bbbc2
Abhishek-S0111 <160207083+Abhishek-S0111@users.noreply.github.com>
2025-07-11 23:48:51 +0530

    TS #4

--- App/app.py
+++ App/app.py
@@ -4,7 +4,8 @@ from recommender import get_recommendations
 st.write("Brother, can i have some oats?")
 movie = st.text_input('Enter a movie name: ')
 
-# if movie:
+if movie:
+    st.write("Hullo!!!")
 #     recommendations = get_recommendations(movie)
 #     st.write(f"Five Movies Similar to '{movie}' are :/n")
 #     for rec in recommendations:
