01e756316548054d0dcfce3769372bdb8a21fd25
Abhishek-S0111 <160207083+Abhishek-S0111@users.noreply.github.com>
2025-07-13 18:40:55 +0530

    Precompute the recommended similar movies for each movie.

--- App/app.py
+++ App/app.py
@@ -1,12 +1,22 @@
 import streamlit as st
-from recommender import get_recommendations
+#from recommender import get_recommendations
+import json
+from pandas import read_csv
 
-st.write("Brother, can i have some oats?")
 movie = st.text_input('Enter a movie name: ')
+
+#load the precompute file
+with open("precompute/movie_sims_dict.json", 'r') as f:
+    map = json.load(f)
+
+#load the movie dataset
+data = read_csv("data/movie.csv")
 
 if movie:
     st.write("Hullo!!!")
-    recommendations = get_recommendations(movie)
-#     st.write(f"Five Movies Similar to '{movie}' are :/n")
-#     for rec in recommendations:
-#         st.write('- ', rec)
+    st.write(f"Five Movies Similar to '{movie}' are :/n")
+    movie_id = data[data['title'] == movie].index[0]
+    similar_id = map[str(movie_id)]
+    recommendations = data.iloc[similar_id]['title'].tolist()
+    for rec in recommendations:
+        st.write('- ', rec)
